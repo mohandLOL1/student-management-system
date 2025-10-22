9391537a6a1a1302aa16f7ad7c6288e5bc18885d
moatasem-1 <mutasemmohamed24@gmail.com>
2025-10-22 17:59:16 +0300

    generates random ID

--- src/main/java/misc/generator.java
+++ src/main/java/misc/generator.java
@@ -1,9 +1,14 @@
 
 package misc;
 
+import java.util.Random;
+
 
 public class generator {
-    public static int randomID(){ //generates random ID from 10000 to 20000
-        return 0;
+    
+   public static int randomID(){ //generates random ID from 10000 to 20000
+        Random rand = new Random();
+        int ID = 10000 + rand.nextInt(10001);
+        return ID;
     }
 }
