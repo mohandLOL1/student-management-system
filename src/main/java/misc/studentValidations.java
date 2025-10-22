e2091d34c1eb38a7f684010fba092ecd3ad8e0fe
moatasem-1 <mutasemmohamed24@gmail.com>
2025-10-22 18:46:00 +0300

    Add files via upload

--- src/main/java/misc/studentValidations.java
+++ src/main/java/misc/studentValidations.java
@@ -5,7 +5,8 @@ import java.io.*;
 
 public class studentValidations {
     public static boolean validateID(int ID) { //random ID, with only digits from 10000 to 20000
-        return true;
+
+      return ID>=10000 && ID<=20000;
     }
 
     public static boolean validateGender(String gender) { //either male/female
@@ -25,23 +26,20 @@ public class studentValidations {
      char[]Department=department.toCharArray();
      
      for(int i=0;i<department.length();i++){
-     if(!Character.isLetter(Department[i]))
-        System.out.println("department not exist");
+       if(!Character.isLetter(Department[i])){
+          System.out.println("department not exist");
         return false;
+       }
      }
      return true;
     }
     
     public static boolean validateGPA(double gpa){ //number from 0 to 4.0
-      if(gpa>=0||gpa<=4.0){
-        return true;}
-      
-      return false;  
+     
+       return gpa>=0 && gpa<=4.0;
     }
     public static boolean validateAge(int age){ //number from 16 to 99
-      if(age>=16||age<=99){
-        return true;}
-      
-      return false;
+        
+       return age>=16 && age<=99;
     }
 }
